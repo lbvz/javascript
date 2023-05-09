169581551a6c2d7c888bc45ffbfdf76bbf8ee73a
lbvz <128460178+lbvz@users.noreply.github.com>
2023-05-09 15:06:30 +0800

    0509

--- c05/js/query-selector.js
+++ c05/js/query-selector.js
@@ -1,8 +1,14 @@
+/*
 let first_element = document.querySelector('li.hot');
-first_element.className = 'cool';
+first_element.className = 'cool';*/
 
 let all_elements = document.querySelectorAll('li.hot');
 console.log(typeof all_elements);
+/*
 all_elements.forEach((current_element) => {
     current_element.className = 'cool';
-})
+})*/
+
+for (let i = 0; i < all_elements.length; i++) {
+    all_elements[i].className = 'cool';
+}
